841be4910fe6acdceedaab6cd26cd3b663d61aa7
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-06-14 15:17:02 +0000

    note on datatype conversion

---
+++ f3_datatypeConversion.js
@@ -0,0 +1,6 @@
+//changing value from string to number 
+let age="12";
+let num1=parseInt(age); //method 1
+let num2=Number(age);   //method 2
+let num3=parseFloat(age) //method3
+console.table([typeof age,typeof num1, typeof num2, typeof num3]);   
