b7bd52fa310411f6c0265f65b6d65b2f7ee56dbf
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-06-14 14:21:33 +0000

    note on variables

---
+++ f1_Variable.js
@@ -0,0 +1,12 @@
+let name='Nikesh';
+/* When we create a varibale using let there is no block scope issue 
+and we can change that value once it have bean set */
+
+
+const age=19;
+/* WHen we create  a variable using const 
+   once the value is set then we cannot change it again*/
+
+
+var gender='m'
+
