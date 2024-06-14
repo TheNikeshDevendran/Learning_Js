6aba739fa32925577b809f50d0782578f0c331e0
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-06-14 14:53:24 +0000

    note on datatypes

---
+++ f2_Datatype.js
@@ -0,0 +1,22 @@
+//  string
+let name='Nieksh';
+
+// number
+let age=19;
+
+//boolean
+let logined=true;
+
+//null
+let address=null;
+
+//undefined
+let phonenumber;
+
+//object
+let data={
+    1:'one',
+    2:'two'
+}
+
+                    z
