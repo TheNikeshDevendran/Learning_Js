c74cdabd18e61abbb4cc662324cb0f106b0ab38c
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-06-21 15:27:14 +0000

    note on number and math

---
+++ f5_numberAndMath.js
@@ -0,0 +1,19 @@
+// numbers and math function
+let num1=new Number(500); //here we are creating the number using number class so when we want 
+// to use this class we will use new keyword to create an object for the number class so that we can use its methods
+// its type will be object not as number
+
+let num2=500; // without using the number class also we can create a number datatype value
+// its type will be number
+// console.log(num2);
+// console.log(typeof num1,typeof num2);
+
+let percent=78.634
+console.log(num2.toString(),//it convert from number to string value
+           percent.toFixed(),//after decimal how many digit do we want to show and it also does round off
+                              //it has range from 0-20
+           percent.toPrecision(1),//it also similar to tofixed function it range from 1-21 
+           
+)
+
+  
