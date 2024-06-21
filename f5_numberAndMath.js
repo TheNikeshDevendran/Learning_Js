b9663c8ee3dc1a271fdc4c8ec4503517c8a38ee9
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-06-21 15:50:01 +0000

    note on number and math

--- f5_numberAndMath.js
+++ f5_numberAndMath.js
@@ -16,4 +16,24 @@ console.log(num2.toString(),//it convert from number to string value
            
 )
 
+// math
+// abs() it make negative value to positive
+//round() it will do round off of given number
+// ceil() it will take next highest value
+// floor() it will take lowest value
+// console.log(Math.abs(-2),
+//         Math.round(4.9),
+//         Math.ceil(4.2),
+//         Math.floor(4.6),
+//         Math.pow(5,2)
+
+
+// );
+
+//this random funciton will generate number from 0-1
+//when we multiply by 10 the decimal get shifted
+//  and when we add it by +1 the zero will not occur because number will start from 1
+console.log(((Math.random()*10)+1).toFixed(0))
+
+
   
