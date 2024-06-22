94be3c75e2c5228a57995e9c2bd683323c91c7be
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-06-22 10:08:58 +0000

    note on date function

--- f6_date.js
+++ f6_date.js
@@ -4,3 +4,11 @@
 let date=new Date()
 console.log(typeof date)//object
 console.log(date.getFullYear())//gives us the specific year
+console.log(date.toLocaleString())//it display date in other pattern
+console.log(date.getDay())//it will give days of the week in number format
+console.log(date.getFullYear())//displays year only
+console.log(date.getDate())//it gives todays date
+let TimeStamp=Date.now() //it will give us date in millisecond
+// console.log(TimeStamp)
+
+
