2038ed0708e79289a9682cf43cd81b8c005a4ff6
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-06-22 12:34:00 +0000

    note on array and its function

---
+++ f7_array.js
@@ -0,0 +1,23 @@
+//array
+// array can contain different types of value in it
+// resisable in nature
+//indexing can be done
+//shallow copy there share same reference when we modify them
+let ar1=[1,-2,true,null,"nikesh"]
+// console.log(ar1)
+let ar2=[20,50,60,1,3]
+// console.log(ar2)
+
+// array methods
+
+// push: add element at last
+// pop:remove element from last
+// unshift:add element at first place of an array that is zero index
+// shift:remove first element from array
+
+let ar3=[2,5,6,7]
+// ar3.push(8)
+// ar3.pop()
+// ar3.unshift(10)
+// ar3.shift()
+console.log(ar3)
