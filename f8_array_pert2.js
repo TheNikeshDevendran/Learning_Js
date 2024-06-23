4f30a9145da59195f6eac36b999e361a8876ca84
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-06-23 05:37:47 +0000

    note on array and its fucntion

--- f8_array_pert2.js
+++ f8_array_pert2.js
@@ -21,4 +21,24 @@ console.log(allHeros)
 // [ 'spidy', 'thor', 'hulk', 'superman', 'batman', 'wonder' ]
 
 let Seprated_Array=[...marvel,...dc]
-console.log(Seprated_Array)
+// console.log(Seprated_Array)
+
+// in this scenario sepeartor methos will not work
+// so we can use flat(specify depth)  method
+//in this array the depth is 3 the depth is calculated by how many internal sub arrya are present
+
+let dum=[1,2,3,true,[4,5,6],null,[7,8,[9,10]]]
+let dum1=[...dum]
+let flated_array=dum.flat(1)//or on depth we can specify Infinity
+// its typeof is object
+// console.log(flated_array)
+
+let num=[2,3,4,5]
+console.log(Array.isArray(num))
+// Array.isArray(num) is a method use to check its an array
+
+let a=10,b=20,c=30
+let d=Array.of(a,b,c)
+console.log(d)
+// Array.of(a,b,c) is a method to make seperate element
+// into single array
