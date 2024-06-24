466afe9f7a7b26254b51bbeaed63d4063ac73b62
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-06-24 15:48:48 +0000

    note on objects

---
+++ f10_objects_part2.js
@@ -0,0 +1,8 @@
+//singleton
+// when we create an onject by using an constructor is called singleton
+// the working all remain same its an way of creating object
+
+let Office=new Object()
+numbers.id=Math.floor(Math.random()*10)
+
+console.log(numbers.id)
