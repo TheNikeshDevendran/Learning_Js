ce007f3d04267d487057928de5b965121fad61e7
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-06-25 17:40:41 +0000

    note on json

---
+++ f11_Json.js
@@ -0,0 +1,11 @@
+// sometime when we get data in json format it contain multiple keys,arrays,and nested Object
+// so it become difficult to understand so we can use online json formatter tool
+// to make them prettier
+
+
+// json:javascript object notation
+// such kind of structure json follow
+//   {
+//     "key":"value",
+//     "key":"value"
+//   } these key and value are in string format
