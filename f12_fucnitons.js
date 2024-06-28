8ba757a0ec35f66ec63f5c4a805eab900da9976f
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-06-28 12:42:57 +0000

    using object,array and rest operator

--- f12_fucnitons.js
+++ f12_fucnitons.js
@@ -21,3 +21,31 @@ function CheckLoginStatus(username){//here username is parameter
 
 CheckLoginStatus('Nikesh')//here the given value is arguments
 CheckLoginStatus()//if we not pass arguments it become undefined
+
+// working with array and object in fucniton
+// here the three dots[...] which we have used is called rest operator
+// by this method a single varibale can store multiple value like args method
+function AddCartPrice(...prices){
+    console.log(prices);
+    console.log(Array.isArray(prices));//it will give true because 
+    //internally it is storing value as array
+}
+// AddCartPrice(40,50);
+
+
+// if i use rest operator in object also it will store multiple
+// objects in array 
+function GiveObject(...ob1){
+    console.log(ob1)
+}
+// GiveObject({1:1,2:2},{3:3,4:4})
+
+function AmazonPriceCalulator(...prices){
+    let Total=0;
+    for(price in prices){
+        Total+=prices[price]
+    }
+    console.log(`The Total of purchased product is ${Total}`);
+}
+
+AmazonPriceCalulator(20,30,40)
