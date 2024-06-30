74ac0eb24323a318981b2275ebd6549c04d46c9f
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-06-30 04:59:45 +0000

    note on stich case

---
+++ f16_switch.js
@@ -0,0 +1,28 @@
+// switch(month){
+//     // case month==1://this structure will not work
+//     //     console.log('one');
+//     //     break;
+    
+//     case 2:
+//         console.log('two');
+//         break;
+    
+//     default://default will work like else conditon
+//         console.log('default executed')
+    
+// }
+
+
+let month=[1,2,3,4,5];
+switch(month){
+    case 1:
+        console.log('one')
+    case 2:
+        console.log('two')
+    case 3:
+        console.log('three')
+    case 4:
+        console.log('four')
+    case 5:
+        console.log('five')
+}
