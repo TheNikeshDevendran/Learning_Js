69cea999d33d53d197b488399d68ca0f8b053033
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-06-30 03:32:19 +0000

    note on control flow

---
+++ f15_controlFlow.js
@@ -0,0 +1,12 @@
+let age=20;
+// if(age>18){
+//     console.log('age is > 18');
+// }
+// else if(age==20){
+//     console.log('age is 20');
+// }
+// if(age==age){
+//     console.log('yes same',age==age);
+// }
+
+console.log(age==20? 'yes':'no')
