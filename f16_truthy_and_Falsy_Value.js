1e777d9d7bd9f815f384962ceb0884e47487c10b
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-07-03 04:29:56 +0000

    note on truthy and falsy values

---
+++ f16_truthy_and_Falsy_Value.js
@@ -0,0 +1,12 @@
+/* falsy values         |    truthy values
+ -----------------------|-------------------                      
+   false                |       "0"
+   0,-0                 |     "false"
+   ''                   |      " "
+   null,undefined       |      [] , {}
+   Nan                  |    function(){}
+   
+*/
+
+
+
