d4a44b5e5b834438d4d1cf533d02fb041e01e7a4
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-07-05 10:03:01 +0000

    note on for loop

--- iterators_in_javascript/01_forLoop.js
+++ iterators_in_javascript/01_forLoop.js
@@ -1,5 +1,14 @@
 for (let i = 0; i <5 ; i++) {
     const element = i;
-    console.log(element);    
+    // console.log(element);    
+}
+ myarray=['john','mike','tom','merry']
+for(let i=0;i<myarray.length;i++){
+    // console.log(myarray[i])
 }
 
+let i=10;
+do{
+ console.log(`I is =${i}`);
+ i++
+}while(i<5)
