46bdca3bdeca6f3eeb372e1198124afce0a7124f
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-07-05 13:46:04 +0000

    note on for-of and for-in loop

---
+++ iterators_in_javascript/03_ForOf_and_ForIn_loop.js
@@ -0,0 +1,10 @@
+let subjects=['english','math','science']
+/* This are two method to make array empty
+   subjects.splice(0,subjects.length);
+   subjects.length=0  */
+   
+for(data of subjects){
+   console.log(data);
+}
+
+
