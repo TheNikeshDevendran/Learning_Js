9bf55dc96a5c5afe73ed918ad4d4338964a50c1b
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-07-05 13:18:50 +0000

    note on while and do while loop

--- iterators_in_javascript/02_while_and_do_while.js
+++ iterators_in_javascript/02_while_and_do_while.js
@@ -1,5 +1,28 @@
-let i=10;
+let temp=10;
+// ++temp
+// temp++;
+// ----
+// here we have used post-increment(++) so after printing the value 
+// of temp it will increment it
+// console.log(temp++,temp);
+
+
+let Table_of=2;
+let i=1;
+// while loop it will first check the condition if its true then it will
+// do the task
+
+// while(i<=10){
+//     console.log(`${Table_of} x ${i} = ${i*Table_of}`);
+//     i++;
+// }
+
+
+// do while loop will first execute after that it will check
+let num=5;
 do{
- console.log(`I is =${i}`);
- i++
-}while(i<5)
+ console.log(`current number is =${i}`);
+ num++
+}while(num<5)
+
+
