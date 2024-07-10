19bc526303a2acbde6b7919d0436836ed4f7d296
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-07-10 13:18:52 +0000

    not on foreach and nested foreach

--- iterators_in_javascript/04_foreach_loop.js
+++ iterators_in_javascript/04_foreach_loop.js
@@ -1,6 +1,45 @@
 let languages=["python","js","cpp","spring"];
 let numbers=[200,3,99,100,50,66]
-numbers.forEach( (num) => {
-    console.log(num%2==0? num+" "+"is even":num+" "+"is odd");
-});
+
+// numbers.forEach( (num) => {
+//     console.log(num%2==0? num+" "+"is even":num+" "+"is odd");
+// });
+
+let Students=[
+    {
+        name:'Nikesh',
+        age:19,
+        subjects:['eng','math','sci']
+    },
+
+    {
+        name:'venkat',
+        age:20,
+        subjects:['bio','math','phy']
+    },
+
+    {
+        name:'sundar',
+        age:35,
+        subjects:['Ml','math','Ds']
+    }
+]
+
+/*Students.forEach((data)=>{
+      console.log(data.name.toUpperCase());
+      let i=1;
+      data.subjects.forEach((sub)=>{
+        console.log(i+":"+sub);
+        i++;
+      });
+});*/
+let i=1;
+ Students.forEach((data)=>{
+     console.log(`${i}${data.name.toUpperCase()}`);
+     data.subjects.forEach((sub)=>{
+         console.log("."+sub);
+     })
+     i++;
+ })
+
 
