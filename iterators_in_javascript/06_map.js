060a9436cde77cd4bb70c00344e9fcd8e180aaae
TheNikeshDevendran <nikeshdevendran07@gmail.com>
2024-07-12 15:12:59 +0000

    note on map and chaining

--- iterators_in_javascript/06_map.js
+++ iterators_in_javascript/06_map.js
@@ -4,3 +4,18 @@ let result1=num.map((num)=>{
 })
 console.log(result1);
 console.log(num);
+// map is also similar to filter functions 
+// ------------
+// CHAINING 
+// it is like multiple funcitons applying to an same object or variable
+
+let num2=[1,2,3,4,5,6,7,8,9];
+let num2_result=num2.map((numbers)=>{
+      return numbers*10;
+}).map((numbers)=>{
+    return numbers*100;
+}).filter((numbers)=>{
+   return numbers<5000;
+})
+
+console.log(num2_result);
